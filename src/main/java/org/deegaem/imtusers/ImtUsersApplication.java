76f875d32e6f939d844ce5f0a0bd3216be6bdade
elsadiq <sadig@gmx.ch>
2021-10-02 11:15:37 +0200

    Initial commit

---
+++ src/main/java/org/deegaem/imtusers/ImtUsersApplication.java
@@ -0,0 +1,13 @@
+package org.deegaem.imtusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImtUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImtUsersApplication.class, args);
+	}
+
+}
